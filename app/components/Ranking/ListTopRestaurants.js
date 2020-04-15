ff81ceb8610922b9b22aaa29f158fc12cf669ff4
valen800 <varulo.cvcv@gmail.com>
2020-04-15 13:24:13 +0200

    Ranking implemented

--- app/components/Ranking/ListTopRestaurants.js
+++ app/components/Ranking/ListTopRestaurants.js
@@ -1,10 +1,121 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { Card, Image, Icon, Rating } from "react-native-elements";
+import * as firebase from "firebase";
 
 export default function LisTopRestaurants(props) {
+  const { restaurants, navigation } = props;
+
   return (
-    <View>
-      <Text>List top restaurants</Text>
-    </View>
+    <FlatList
+      data={restaurants}
+      renderItem={(restaurant) => (
+        <Restaurant restaurant={restaurant} navigation={navigation} />
+      )}
+      keyExtractor={(item, index) => index.toString()}
+    />
   );
 }
+
+function Restaurant(props) {
+  const { restaurant, navigation } = props;
+  const { name, description, images, rating } = restaurant.item;
+  const [imageRestaurant, setImageRestaurant] = useState(null);
+  const [iconColor, setIconColor] = useState("#000");
+
+  useEffect(() => {
+    const image = images[0];
+    firebase
+      .storage()
+      .ref(`restaurant-images/${image}`)
+      .getDownloadURL()
+      .then((response) => {
+        setImageRestaurant(response);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (restaurant.index === 0) {
+      setIconColor("#ffbf00");
+    } else if (restaurant.index === 1) {
+      setIconColor("#8a9597");
+    } else if (restaurant.index === 2) {
+      setIconColor("#763c28");
+    }
+  }, []);
+
+  return (
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate("Restaurant", {
+          restaurant: restaurant.item,
+        })
+      }
+    >
+      <Card containerStyle={styles.containerCard}>
+        <Icon
+          type="material-community"
+          name="chess-queen"
+          color={iconColor}
+          size={40}
+          containerStyle={styles.containerIcon}
+        />
+        <Image
+          style={styles.restaurantImage}
+          resizeMode="cover"
+          source={{ uri: imageRestaurant }}
+        />
+        <View style={styles.titleRating}>
+          <Text style={styles.title}>{name}</Text>
+          <Rating
+            imageSize={20}
+            startingValue={rating}
+            readonly
+            style={styles.rating}
+          />
+        </View>
+        <Text style={styles.description}>{description}</Text>
+      </Card>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerIcon: {
+    position: "absolute",
+    top: -30,
+    left: -30,
+    zIndex: 1,
+  },
+  containerCard: {
+    marginBottom: 30,
+    borderWidth: 0,
+  },
+  restaurantImage: {
+    height: 200,
+    width: "100%",
+  },
+  titleRating: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  rating: {
+    position: "absolute",
+    right: 0,
+  },
+  description: {
+    color: "grey",
+    marginTop: 0,
+    textAlign: "justify",
+  },
+});
--- app/screens/TopRestaurants.js
+++ app/screens/TopRestaurants.js
@@ -9,27 +9,31 @@ import {
 } from "react-native";
 import { Card, Image, Rating } from "react-native-elements";
 import Toast from "react-native-easy-toast";
+import ListTopRestaurants from "../components/Ranking/ListTopRestaurants";
 
 import { firebaseApp } from "../utils/FireBase";
 import firebase from "firebase/app";
 import "firebase/firestore";
 const db = firebase.firestore(firebaseApp);
 
-export default function TopRestaurants() {
+export default function TopRestaurants(props) {
+  const { navigation } = props;
   const [restaurants, setRestaurants] = useState([]);
   const toastRef = useRef();
 
   useEffect(() => {
     (async () => {
       db.collection("restaurants")
-        .orderBy("rating")
+        .orderBy("rating", "desc")
         .limit(5)
         .get()
         .then((response) => {
           const restaurantArray = [];
 
           response.forEach((doc) => {
-            restaurantArray.push(doc.data());
+            let restaurant = doc.data();
+            restaurant.id = doc.id;
+            restaurantArray.push(restaurant);
           });
           setRestaurants(restaurantArray);
         })
@@ -45,7 +49,7 @@ export default function TopRestaurants() {
 
   return (
     <View>
-      <Text>We're in Top Restaurants</Text>
+      <ListTopRestaurants restaurants={restaurants} navigation={navigation} />
       <Toast ref={toastRef} position="center" opacity={1} />
     </View>
   );
